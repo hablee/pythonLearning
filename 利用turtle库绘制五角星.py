5bf8841682022e78c69c1414d54297718796f584
hablee <46622299+hablee@users.noreply.github.com>
2019-10-01 01:27:14 +0800

    Create 利用turtle库绘制五角星.py

---
+++ 利用turtle库绘制五角星.py
@@ -0,0 +1,12 @@
+#利用turtle库绘制五角星
+import turtle
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.done()
